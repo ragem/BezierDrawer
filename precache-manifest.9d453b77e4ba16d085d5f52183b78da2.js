4f4e9b87c353ab7e3fccb93906c9db8969babeed
Edgars Kuzmanis <edgars.kuzmanis@scandiweb.com>
2019-11-12 08:51:39 +0200

    Updates

---
+++ precache-manifest.9d453b77e4ba16d085d5f52183b78da2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1714cff983edc2e2cf4f9d8c9fd875f",
+    "url": "/Circledrawer/index.html"
+  },
+  {
+    "revision": "5f7896ca53a28532a79d",
+    "url": "/Circledrawer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "bc948d09cdaea8184946",
+    "url": "/Circledrawer/static/js/2.ddf8ebce.chunk.js"
+  },
+  {
+    "revision": "5f7896ca53a28532a79d",
+    "url": "/Circledrawer/static/js/main.7210344f.chunk.js"
+  },
+  {
+    "revision": "a6740abc240b527f42f0",
+    "url": "/Circledrawer/static/js/runtime-main.92ade257.js"
+  }
+]);
